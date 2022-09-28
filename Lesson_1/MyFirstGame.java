3d5025eb806cb4cc15b3de92af2e880c473646c0
Nastya <Nastya>
2022-09-28 12:46:01 +0300

    'FixComments28/09/2022'

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,20 +2,21 @@ public class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("Угадай число");
         int randomNum = 63;
-        int lowNum = 1;
-        int highNum = 100;
-        while (lowNum <= highNum) {
-            int midNum = lowNum + ((highNum - lowNum) / 2);
+        int startRange = 1;
+        int endRange = 100;
+        int midNum = 0;
+        while (startRange != endRange) {
+            midNum = startRange + ((endRange - startRange) / 2);
             if (midNum < randomNum) {
                 System.out.println("Число " + midNum + " меньше того, что загадал компьютер");
-                lowNum = midNum + 1;
+                startRange = midNum + 1;
             } else if (midNum > randomNum) {
                 System.out.println("Число " + midNum + " больше того, что загадал компьютер");
-                highNum = midNum - 1;
-            } else if (midNum == randomNum) {
-                System.out.println("Вы победили! Загаданное число: " + midNum);
+                endRange = midNum - 1;
+            } else {
                 break;
             }
         }
+        System.out.println("Вы победили! Загаданное число: " + midNum);
     }
 }

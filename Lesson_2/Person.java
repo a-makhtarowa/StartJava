a2d22a83f1cc33284b67ca286a62b8c31f22bba0
Nastya <Nastya>
2022-10-06 23:12:30 +0300

    NewFile06/10/2022

---
+++ Lesson_2/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    String sex = man;
+    String name = Andrey;
+    double weight = 80;
+    int age = 30;
+
+    void go() {
+        System.out.println("Go!");
+    }
+
+    void sit() {
+        System.out.println("Sit!");
+    }
+
+    void run() {
+        System.out.println("Run!");
+    }
+
+    void talk() {
+        System.out.println("Talk to me!");
+    }
+
+    void learnJava() {
+        System.out.println("Learn faster!");
+    }
+}

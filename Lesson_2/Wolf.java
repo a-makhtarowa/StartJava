e293dc92f8b3b2307964ebfa3f17bf832235f697
Nastya <Nastya>
2022-10-07 22:54:49 +0300

    NewFile07/10/2022

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    String sex;
+    String name;
+    double weight;
+    int age;
+    String color;
+
+    void go() {
+        System.out.println("Go!");
+    }
+
+    void sit() {
+        System.out.println("Sit!");
+    }
+
+    void run() {
+        System.out.println("Run!");
+    }
+
+    void howl() {
+        System.out.println("Howl louder!");
+    }
+
+    void hunt() {
+        System.out.println("Hunt!");
+    }
+}

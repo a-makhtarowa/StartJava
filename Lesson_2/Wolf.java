4d82a902a7c88d41d8dddf7b4103f22181b87ecc
Nastya <Nastya>
2022-10-12 00:29:30 +0300

    NewFile12/10/2022

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,66 @@
 public class Wolf {
-    String sex;
-    String name;
-    double weight;
-    int age;
-    String color;
+    private String sex;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
 
-    void go() {
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void go() {
         System.out.println("Go!");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Sit!");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Run!");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("Howl louder!");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Hunt!");
     }
 }

afabf671d57d685d8ea7be03b8c9052032475b26
Nastya <Nastya>
2022-10-15 23:36:23 +0300

    FixComments15/10/2022

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -2,61 +2,40 @@ public class Calculator {
     private int num1;
     private int num2;
     private char sign;
-    private long result = 1;
 
     public void setNum1(int num1) {
         this.num1 = num1;
-    }
-
-    public int getNum1() {
-        return num1;
     }
 
     public void setNum2(int num2) {
         this.num2 = num2;
     }
 
-    public int getNum2() {
-        return num2;
-    }
-
     public void setSign(char sign) {
         this.sign = sign;
     }
 
-    public char getSign() {
-        return sign;
-    }
-
-    public long getResult() {
-        return result;
-    }
-
     public long calculate() {
+        long result = 1;
         switch (sign) {
             case '+':
-                result = num1 + num2;
-                break;
+                return num1 + num2;
             case '-':
-                result = num1 - num2;
-                break;
+                return num1 - num2;
             case '*':
-                result = num1 * num2;
-                break;
+                return num1 * num2;
             case '/':
-                result = num1 / num2;
-                break;
+                return num1 / num2;
             case '^':
                 for (int i = 0; i < num2; i++) {
                     result *= num1;
                 }
-                break;
+                return result;
             case '%':
-                result = num1 % num2;
-                break;
+                return num1 % num2;
             default:
                 System.out.println("Неизвестная операция");
+                return 0;
         }
-        return result;
     }
 }

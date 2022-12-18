e5f5afcc899111449738cca01c18393ed2a06945
NastyaMakhtarova <NastyaMakhtarova>
2022-12-18 22:12:52 +0100

    FixComments18/12/2022

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -7,34 +7,22 @@ public class Calculator {
         int num1 = Integer.parseInt(elements[0]);
         char sign = elements[1].charAt(0);
         int num2 = Integer.parseInt(elements[2]);
-        findException(num1);
-        findException(num2);
-        switch (sign) {
-            case '+' -> {
-                return Math.addExact(num1, num2);
-            }
-            case '-' -> {
-                return Math.subtractExact(num1, num2);
-            }
-            case '*' -> {
-                return Math.multiplyFull(num1, num2);
-            }
-            case '/' -> {
-                return Math.floorDiv(num1, num2);
-            }
-            case '^' -> {
-                return (long) Math.pow(num1, num2);
-            }
-            case '%' -> {
-                return (long) Math.IEEEremainder(num1, num2);
-            }
+        checkNumberIsPositive(num1);
+        checkNumberIsPositive(num2);
+        return switch (sign) {
+            case '+' -> Math.addExact(num1, num2);
+            case '-' -> Math.subtractExact(num1, num2);
+            case '*' -> Math.multiplyFull(num1, num2);
+            case '/' -> Math.floorDiv(num1, num2);
+            case '^' -> (long) Math.pow(num1, num2);
+            case '%' -> (long) Math.IEEEremainder(num1, num2);
             default -> {
                 System.out.println("Неизвестная операция");
-                return 0;
+                yield 0;
             }
-        }
+        };
     }
-    private static void findException(int num) {
+    private static void checkNumberIsPositive(int num) {
         if (num <= 0) {
             throw new RuntimeException("Число " + num + " не положительное");
         }
